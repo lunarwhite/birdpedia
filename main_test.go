f990b767ef2714e0e40a4ad080f9780489245417
lunarwhite <dwcn22@outlook.com>
2021-11-20 12:21:52 +0800

    create test for TestStaticFileServer

--- main_test.go
+++ main_test.go
@@ -67,7 +67,26 @@ func TestRouterForNonExistentRoute(t *testing.T) {
 	if respString != expected {
 		t.Errorf("Response should be %s, got %s", expected, respString)
 	}
+}
 
+func TestStaticFileServer(t *testing.T) {
+	r := newRouter()
+	mockServer := httptest.NewServer(r)
+
+	resp, err := http.Get(mockServer.URL + "/assets/")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Status should be 200, got %d", resp.StatusCode)
+	}
+
+	contentType := resp.Header.Get("Content-Type")
+	expectedContentType := "text/html; charset=utf-8"
+	if expectedContentType != contentType {
+		t.Errorf("Wrong content type, expected %s, got %s", expectedContentType, contentType)
+	}
 }
 
 func TestHandler(t *testing.T) {
