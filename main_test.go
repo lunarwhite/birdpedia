8c97b0e033692f72e5e1a42dcc09373abaedb818
lunarwhite <dwcn22@outlook.com>
2021-11-20 11:42:36 +0800

    separate route constructor function

--- main_test.go
+++ main_test.go
@@ -1,13 +1,46 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
+func TestRouter(t *testing.T) {
+	r := newRouter()
+	mockServer := httptest.NewServer(r)
+
+	// The mock server we created runs a server and exposes its location in the URL attribute.
+	// We make a GET request to the "hello" route we defined in the router.
+	resp, err := http.Get(mockServer.URL + "/hello")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	// We want our status to be 200 (ok).
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Status should be ok, got %d", resp.StatusCode)
+	}
+
+	// In the next few lines, the response body is read, and converted to a string.
+	defer resp.Body.Close()
+
+	// Read the body into a bunch of bytes (b).
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Convert the bytes to a string.
+	respString := string(b)
+	expected := "Hello World!"
+	if respString != expected {
+		t.Errorf("Response should be %s, got %s", expected, respString)
+	}
+}
+
 func TestHandler(t *testing.T) {
-	//Here, we form a new HTTP request. This is the request that's going to be passed to our handler.
+	// Here, we form a new HTTP request. This is the request that's going to be passed to our handler.
 	// The first argument is the method, the second argument is the route,
 	// and the third is the request body, which we don't have in this case.
 	req, err := http.NewRequest("GET", "", nil)
