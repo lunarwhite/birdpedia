5045bf5347c5523ccbe883857d2387b3721af9c3
lunarwhite <dwcn22@outlook.com>
2021-11-20 10:56:31 +0800

    update for mux lib

--- main.go
+++ main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	// Declare a new router
+	r := mux.NewRouter()
+
+	// The router allows us to declare methods that this path will be valid for
+	r.HandleFunc("/hello", handler).Methods("GET")
+
+	// We can then pass our router to this method
+	http.ListenAndServe(":8080", r)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
